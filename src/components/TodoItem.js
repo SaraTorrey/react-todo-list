d5dd1bd4396f333bc3732be01c416af137605398
Sara Torrey <sara.moravej@gmail.com>
2019-04-19 16:54:01 -0500

    Add files missed in the last commit

---
+++ src/components/TodoItem.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import { gray } from 'ansi-colors';
+
+export class TodoItem extends Component {
+    getStyle = () => {
+        return {
+            background: '#f4f4f4',
+            padding: '10px',
+            borderBottom: '1px #ccc dotted',
+            textDecoration: this.props.todo.completed ?
+            'line-through' : 'none'
+        } 
+    }
+    markComplete = (e) => {
+        console.log(this.props)
+    } 
+    render() {
+        const { id,title } = this.props.todo;
+        return (
+             <div style={this.getStyle()}>
+                 <p>
+                    <input type='checkbox' onChange={this.props.markComplete.bind(this, id)} /> {' '}
+                    { title }
+                 </p> 
+             </div>
+        );
+    }
+} 
+TodoItem.protoTypes ={
+    todo: PropTypes.object.isRequired
+}
+
+
+export default TodoItem
