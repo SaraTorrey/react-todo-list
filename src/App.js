1c2e7a04ca41737b0822f379b906a42b8beec334
Sara Torrey <sara.moravej@gmail.com>
2019-04-23 15:53:54 -0500

    Add React Router and About page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 import Header from './components/layout/Header';
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo'
+import About from './components/pages/About'
 import uuid from 'uuid';
 
 class App extends Component {
@@ -53,13 +55,20 @@ class App extends Component {
 
   render() {
     return (
+      <Router>
       <div className="App">
         <div className="container">
-          <Header/>
-          <AddTodo addTodo={this.addTodo} />
-          <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
+          <Header />
+          <Route path="/" render={props => (
+            <React.Fragment>
+              <AddTodo addTodo={this.addTodo} />
+              <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
+            </React.Fragment>
+          )} />}
+          <Route path="/about" Component={About} />
         </div>
       </div>
+      </Router>
     );
   }
 }
