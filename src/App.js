89a90f81ab9b153a8149267633ac9f95a386f0ba
Sara Torrey <sara.moravej@gmail.com>
2019-04-23 20:07:40 -0500

    Change casing of folder pages

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 import Header from './components/layout/Header';
 import Todos from './components/Todos';
-import AddTodo from './components/AddTodo'
-import About from './components/pages/About'
+import AddTodo from './components/AddTodo';
+import About from './components/pages/About';
 import uuid from 'uuid';
 
 class App extends Component {
@@ -65,7 +65,7 @@ class App extends Component {
               <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
             </React.Fragment>
           )} />}
-          <Route path="/about" Component={About} />
+          <Route path="/about" component={About} />
         </div>
       </div>
       </Router>
